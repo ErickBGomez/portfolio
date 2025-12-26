7ca14be0b03c5518afd19178d8b8edac39ffb123
ErickBGomez <erickbgomez.dev@gmail.com>
2025-12-26 09:31:36 -0600

    Replace div with nav container for semantic tag in header

--- src/layout/components/header/Header.tsx
+++ src/layout/components/header/Header.tsx
@@ -12,11 +12,11 @@ const Header = () => {
   return (
     <div className="w-full p-6">
       <div className="max-w-250 mx-auto">
-        <div className="flex items-center justify-between">
+        <nav className="flex items-center justify-between">
           {navElements.map((element, index) => (
             <NavElement key={index} label={element.label} href={element.href} />
           ))}
-        </div>
+        </nav>
       </div>
     </div>
   );
