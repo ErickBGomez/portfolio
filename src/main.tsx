e2127c1513fff028bf177a04fc6062696bdd0890
ErickBGomez <erickbgomez.dev@gmail.com>
2025-12-25 21:31:29 -0600

    Define base styles with tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./pages/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./layout/Layout";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
